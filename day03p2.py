28ccc92cd50e2311e349cce27e2fff44e6c9cf9f
Ritesh <ritesh.sathiyamoorthi@gmail.com>
2025-12-03 18:32:26 +0800

    Made improvements to day 3 part 2

--- day03p2.py
+++ day03p2.py
@@ -1,34 +1,24 @@
 import sys
-from functools import cache
 
-
-@cache
-def volt_dfs(complete_bank, remaining_bank, current_volts, max_depth):
+def volt_search(complete_bank, remaining_bank, current_volts, max_depth):
     if len(current_volts) == max_depth:
         return int(current_volts)
 
     assert len(current_volts) < max_depth, f"{complete_bank}, {remaining_bank}, {current_volts}, {max_depth}"
 
     volts_required = max_depth - len(current_volts)
-    
-    highest_volt = 0
-    
+
     next_volts = []
-    
+
     for j in range((len(remaining_bank) - volts_required) + 1):
         next_volts.append((j, remaining_bank[j]))
 
     max_next_volt = max(next_volts, key=lambda v: int(v[1]))[1]
-    
-    for j, next_volt in next_volts:
-        if next_volt != max_next_volt:
-            continue
-        
-        possible_highest_volt = volt_dfs(complete_bank, remaining_bank[j+1:], current_volts + next_volt, max_depth)
-        highest_volt = max(possible_highest_volt, highest_volt)
-        
-    return highest_volt
 
+    # Now we get the volt for the lowest J to get the best volt
+    next_volt = min([volts for volts in next_volts if volts[1] == max_next_volt], key=lambda v: v[0])
+
+    return volt_search(complete_bank, remaining_bank[next_volt[0] + 1 :], current_volts + next_volt[1], max_depth)
 
 
 def main():
@@ -44,8 +34,7 @@ def main():
     max_volt = 0
     banks = puzzle_input.split("\n")
     for bank in banks:
-        max_volt += volt_dfs(bank, bank, "", 12)
-        volt_dfs.cache_clear()
+        max_volt += volt_search(bank, bank, "", 12)
 
     print(max_volt)
 
